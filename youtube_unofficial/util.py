57553e8bd97d5e6296fbb64fae9db748565eb16f
Andrew Udvare <audvare@gmail.com>
2019-03-08 15:25:29 -0500

    util: add missing imports

--- youtube_unofficial/util.py
+++ youtube_unofficial/util.py
@@ -1,9 +1,50 @@
-__all__ = ['compat_str', 'try_get', 'remove_start', 'html_hidden_inputs']
+import re
 
 try:
     compat_str = unicode  # Python 2
+    from HTMLParser import HTMLParser as compat_HTMLParser
 except NameError:
     compat_str = str
+    from html.parser import HTMLParser as compat_HTMLParser
+
+
+__all__ = ['compat_str', 'try_get', 'remove_start', 'html_hidden_inputs']
+
+
+class HTMLAttributeParser(compat_HTMLParser):
+    """Trivial HTML parser to gather the attributes for a single element"""
+    def __init__(self):
+        self.attrs = {}
+        compat_HTMLParser.__init__(self)
+
+    def handle_starttag(self, tag, attrs):
+        self.attrs = dict(attrs)
+
+
+def extract_attributes(html_element):
+    """Given a string for an HTML element such as
+    <el
+         a="foo" B="bar" c="&98;az" d=boz
+         empty= noval entity="&amp;"
+         sq='"' dq="'"
+    >
+    Decode and return a dictionary of attributes.
+    {
+        'a': 'foo', 'b': 'bar', c: 'baz', d: 'boz',
+        'empty': '', 'noval': None, 'entity': '&',
+        'sq': '"', 'dq': '\''
+    }.
+    NB HTMLParser is stricter in Python 2.6 & 3.2 than in later versions,
+    but the cases in the unit test will work for all of 2.6, 2.7, 3.2-3.5.
+    """
+    parser = HTMLAttributeParser()
+    try:
+        parser.feed(html_element)
+        parser.close()
+    # Older Python may throw HTMLParseError in case of malformed HTML
+    except compat_HTMLParseError:
+        pass
+    return parser.attrs
 
 
 def try_get(src, getter, expected_type=None):
