2a9f6ef4160ce3643c0dc764d771431173c7ca07
Andrew Udvare <audvare@gmail.com>
2019-07-02 09:47:58 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='youtube-unofficial',
-      version='0.1.0',
+      version='0.2.0',
       author='Andrew Udvare',
       author_email='audvare@gmail.com',
       packages=['youtube_unofficial'],
