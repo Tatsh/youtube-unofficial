24724b8e3b2b109c1f14e307fb084ad3ddbc24a2
Andrew Udvare <audvare@gmail.com>
2021-01-20 18:46:46 -0500

    youtube_unofficial/initial: updates

--- youtube_unofficial/initial.py
+++ youtube_unofficial/initial.py
@@ -7,22 +7,19 @@ from bs4 import BeautifulSoup as Soup
 from .typing.guide_data import GuideData
 from .util import first
 
+YT_INITIAL_DATA_RE = r'^var ytInitialData(?:\s+)?='
+
 
 def initial_data(content: Soup) -> Mapping[str, Any]:
-    text = first(
-        filter(lambda x: '"ytInitialData"' in x.text,
-               content.select('script'))).text.strip()
-    if 'JSON.parse' in text:
-        return cast(
-            Mapping[str, Any],
-            json.loads(
-                first(json.JSONDecoder().raw_decode(
-                    first(
-                        re.sub(r'^window[^=]+= JSON\.parse\(', '',
-                               text).split('\n'))[:-1]))))
     return cast(
         Mapping[str, Any],
-        json.loads(first(re.sub('^window[^=]+= ', '', text).split('\n'))[:-1]))
+        json.loads(
+            first(
+                re.sub(
+                    YT_INITIAL_DATA_RE, '',
+                    first(x.text.strip() for x in content.select('script')
+                          if re.match(YT_INITIAL_DATA_RE,
+                                      x.text.strip()))).split('\n'))[:-1]))
 
 
 def initial_guide_data(content: Soup) -> GuideData:
@@ -30,9 +27,7 @@ def initial_guide_data(content: Soup) -> GuideData:
         GuideData,
         first(
             json.loads(
-                re.sub(
-                    '^var ytInitialGuideData = ', '',
-                    first(
-                        filter(lambda x: 'var ytInitialGuideData =' in x.text,
-                               content.select('script'))).text.strip()).split(
-                                   '\n')))[:-1])
+                first(
+                    first(x.text for x in content.select('script')
+                          if 'var ytInitialGuideData =' in
+                          x.text).strip().split('\n')[:-1]))))
