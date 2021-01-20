74baf421ce6032d41897ea4f81d2054cef630502
Andrew Udvare <audvare@gmail.com>
2021-01-20 18:56:43 -0500

    youtube_unofficial/downloaders: pass unknown arguments to youtube-dl (after --)

--- youtube_unofficial/downloaders.py
+++ youtube_unofficial/downloaders.py
@@ -1,5 +1,5 @@
 from os import chdir, makedirs
-from typing import Any, Dict, Optional, cast
+from typing import Any, Dict, Iterable, Optional, cast
 import subprocess as sp
 import sys
 
@@ -7,11 +7,15 @@ from youtube_unofficial.scripts import get_common_parser, parse_common_args
 
 from . import YouTube
 
-__all__ = ('download_history', 'download_playlist', 'download_watch_later')
+__all__ = ('download_history', 'download_playlist', 'download_watch_later',
+           'download_liked')
 
 
-def call_youtube_dl(video_id: str) -> sp.CompletedProcess:
-    return sp.run(('youtube-dl', '--', video_id), check=True)
+def call_youtube_dl(
+        video_id: str,
+        args: Optional[Iterable[str]] = None) -> sp.CompletedProcess:
+    args = tuple(args) if args else tuple()
+    return sp.run(('youtube-dl', ) + args + ('--', video_id), check=True)
 
 
 def download_history() -> int:
@@ -21,7 +25,7 @@ def download_history() -> int:
                         '--delete-after',
                         action='store_true',
                         help='Delete each downloaded item from history')
-    args = parser.parse_args()
+    args, ytdl_args = parser.parse_known_args()
     kwargs = cast(Dict[str, Any], parse_common_args(args))
     kwargs['logged_in'] = True
     yt = YouTube(**kwargs)
@@ -35,7 +39,7 @@ def download_history() -> int:
     makedirs(args.output_dir[0], exist_ok=True)
     chdir(args.output_dir[0])
     for item in yt.get_history_info():
-        call_youtube_dl(item['videoRenderer']['videoId'])
+        call_youtube_dl(item['videoRenderer']['videoId'], ytdl_args[1:])
         if args.delete_after:
             yt.remove_video_ids_from_history(item['videoRenderer']['videoId'])
     return 0
@@ -51,7 +55,7 @@ def download_playlist(playlist_id: Optional[str] = None) -> int:
                         action='store_true',
                         help='Delete each downloaded item from history')
     parser.add_argument('playlist_id', nargs=1, default=playlist_id)
-    args = parser.parse_args()
+    args, ytdl_args = parser.parse_known_args()
     kwargs = cast(Dict[str, Any], parse_common_args(args))
     kwargs['logged_in'] = True
     yt = YouTube(**kwargs)
@@ -68,7 +72,7 @@ def download_playlist(playlist_id: Optional[str] = None) -> int:
     chdir(args.output_dir[0])
     for item in yt.get_playlist_info(args.playlist_id[0]):
         renderer = item['playlistVideoRenderer']
-        call_youtube_dl(renderer['videoId'])
+        call_youtube_dl(renderer['videoId'], ytdl_args[1:])
         if args.delete_after:
             yt.remove_set_video_id_from_playlist(args.playlist_id[0],
                                                  renderer['setVideoId'])
